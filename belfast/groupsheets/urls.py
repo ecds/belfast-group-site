75fdd021af94e753b20ba61acad09f6bf11afceb
rlskoeser <rebecca.s.koeser@emory.edu>
2013-05-23 17:14:17 -0400

    first-pass view to display list of all group sheets [#49295429]

--- belfast/groupsheets/urls.py
+++ belfast/groupsheets/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 from belfast.groupsheets import views
 
 urlpatterns = patterns('',
+    url(r'^$', views.list, name='list'),
     url(r'^(?P<id>[a-z0-9_-]+)/$', views.view_sheet, name='view'),
 )
--- belfast/groupsheets/views.py
+++ belfast/groupsheets/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import Http404
 from eulexistdb.exceptions import DoesNotExist
 
-from belfast.groupsheets.models import GroupSheet
+from belfast.groupsheets.models import GroupSheet, get_rdf_groupsheets
 
 
 def view_sheet(request, id):
@@ -12,3 +12,9 @@ def view_sheet(request, id):
         raise Http404
     return render(request, 'groupsheets/display.html',
                  {'document': gs})
+
+def list(request):
+    # use rdf to generate a list of belfast group sheets
+    results = get_rdf_groupsheets()
+    return render(request, 'groupsheets/list.html',
+        {'documents': results})
