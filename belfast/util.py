47d3ee1ec40a5a1cc603ed652cde3bfd8301aa23
rlskoeser <rebecca.s.koeser@emory.edu>
2013-06-21 18:35:40 -0400

    cache rdf data once loaded & parsed; add access to networkx graph data [#49309035]

--- belfast/util.py
+++ belfast/util.py
@@ -3,36 +3,49 @@ import rdflib
 import logging
 from os import path
 import time
+import networkx as nx
+from networkx.readwrite import gexf
 
 from django.conf import settings
+from django.core.cache import cache
 
 logger = logging.getLogger(__name__)
 
-_RDF_GRAPH = None
-
-
 def rdf_data():
-    global _RDF_GRAPH
-    if _RDF_GRAPH is None:
+    cache_key = 'BELFAST_RDF_GRAPH'
+    timeout = 60 * 60 * 24
+    graph = cache.get(cache_key)
+    if graph is None:
         start = time.time()
 
-        g = rdflib.Graph()
+        graph = rdflib.Graph()
         filecount = 0
         # TODO: log time it takes to parse and load, totalnumber of files
         for infile in glob.iglob(path.join(settings.RDF_DATA_DIR, '*.xml')):
-            g.parse(infile)
+            graph.parse(infile)
             filecount += 1
 
         for infile in glob.iglob(path.join(settings.RDF_DATA_DIR, '*', '*.rdf')):
-            g.parse(infile)
+            graph.parse(infile)
             filecount += 1
 
         logger.debug('Loaded %d RDF documents in %.02f sec' % (filecount,
                      time.time() - start))
 
-        _RDF_GRAPH = g
+        cache.set(cache_key, graph, timeout)
 
-    return _RDF_GRAPH
+    return graph
     # load related info (viaf, dbpedia, geonames)
     # for infile in glob.iglob(path.join(settings.RDF_DATA_DIR, 'viaf/*.rdf')):
     #     g.parse(infile)
+
+
+_NX_GRAPH = None
+
+
+def network_data():
+    global _NX_GRAPH
+    if _NX_GRAPH is None:
+        _NX_GRAPH = gexf.read_gexf(settings.GEXF_DATA)
+    return _NX_GRAPH
+
