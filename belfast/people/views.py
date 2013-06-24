2caf6f9c34ba45c8fd96978be4313bc175374c94
rlskoeser <rebecca.s.koeser@emory.edu>
2013-06-24 11:09:47 -0400

    better error handling/type checking for profile view

--- belfast/people/views.py
+++ belfast/people/views.py
@@ -3,7 +3,7 @@ from django.http import Http404
 import rdflib
 
 from belfast.people.models import get_belfast_people
-from belfast.groupsheets.models import RdfPerson
+from belfast.groupsheets.models import RdfPerson, SCHEMA_ORG
 from belfast.util import rdf_data
 
 
@@ -15,13 +15,20 @@ def list(request):
 
 
 def profile(request, id):
-    idtype, idnum = id.split(':')
+    person = None
+    try:
+        idtype, idnum = id.split(':')
+    except:
+        raise Http404
+
     if idtype == 'viaf':
         uri = 'http://viaf.org/viaf/%s/' % idnum
         graph = rdf_data()
-        person = RdfPerson(graph, rdflib.URIRef(uri))
-    else:
-        # anything else not found for now
+        if (rdflib.URIRef(uri), rdflib.RDF.type, SCHEMA_ORG.Person) in graph:
+            person = RdfPerson(graph, rdflib.URIRef(uri))
+
+    # anything else not found for now
+    if person is None:
         raise Http404
 
     return render(request, 'people/profile.html', {'person': person})
