267d637cad9b6253785a2fb51630368560e21fb9
rlskoeser <rebecca.s.koeser@emory.edu>
2013-06-24 11:10:09 -0400

    simple, preliminary tests for profile view

--- belfast/people/tests.py
+++ belfast/people/tests.py
@@ -1,16 +1,24 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class PeopleViewsTest(TestCase):
+
+    def test_profile(self):
+        # currently, id format is viaf:####
+        # - not in domain:id format
+        response = self.client.get(reverse('people:profile',
+                                           kwargs={'id': 'not-a-viaf-id'}))
+        self.assertEqual(404, response.status_code,
+                         'profile should return 404 for invalid person id')
+        # - domain not viaf
+        response = self.client.get(reverse('people:profile',
+                                           kwargs={'id': 'dbpedia:1234'}))
+        self.assertEqual(404, response.status_code,
+                         'profile should return 404 for unsupported person id')
+        # valid viaf id but not a person
+        response = self.client.get(reverse('people:profile',
+                                           kwargs={'id': 'viaf:127261399'}))
+        self.assertEqual(404, response.status_code,
+                         'profile should return 404 for non-person id')
+
