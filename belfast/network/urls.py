ced44a2feadb80f56b5d37cd0ba0f412eeaeed3e
rlskoeser <rebecca.s.koeser@emory.edu>
2013-07-02 16:30:08 -0400

    first-pass graph of just people & belfast group [#52558815]

--- belfast/network/urls.py
+++ belfast/network/urls.py
@@ -4,4 +4,6 @@ from belfast.network import views
 urlpatterns = patterns('',
     url(r'^$', views.full, name='full'),
     url(r'^full.json$', views.full_js, name='full-js'),
+    url(r'^belfast-group/$', views.group_people, name='bg'),
+    url(r'^belfast-group.json$', views.group_people_js, name='bg-js'),
 )
