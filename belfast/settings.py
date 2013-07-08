552aad22787ddbebf20882457eddf3d88a6054ca
rlskoeser <rebecca.s.koeser@emory.edu>
2013-07-08 16:39:48 -0400

    add rdfa middleware (borrowed from findingaids) to make it easier to review embedded rdfa [#49305799]

--- belfast/settings.py
+++ belfast/settings.py
@@ -84,6 +84,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'belfast.rdf_middleware.RDFaMiddleware'
 )
 
 ROOT_URLCONF = 'belfast.urls'
