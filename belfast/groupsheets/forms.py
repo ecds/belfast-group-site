5cbcca54fcb8e41bfce22118ff64592647abd538
rlskoeser <rebecca.s.koeser@emory.edu>
2013-07-08 11:10:00 -0400

    add search form validation to reject search terms with leading wildcards [Fixes #49297345]

--- belfast/groupsheets/forms.py
+++ belfast/groupsheets/forms.py
@@ -1,6 +1,5 @@
 from django import forms
-from django.core.validators import RegexValidator
-from django.core.exceptions import ValidationError
+from django.utils.safestring import mark_safe
 from eulcommon.searchutil import search_terms
 
 
@@ -17,13 +16,19 @@ class KeywordSearchForm(forms.Form):
             'data-toggle': 'tooltip',
             'data-placement': 'bottom',
             'data-original-title': kw_help
-        })
+        }),
+        error_messages={'required': 'No search terms were entered.'}
     )
 
     def clean_keywords(self):
         data = self.cleaned_data['keywords']
         # doesn't care about mis-matched quotes, just strips them out
         terms = search_terms(data)
+        for t in terms:
+            if t.startswith('*') or t.startswith('?'):
+                raise forms.ValidationError(
+                    mark_safe('Search terms may not begin with wildcards <b>*</b> or <b>?</b>'),
+                    code='invalid')
         # NOTE: this cleans up mismatched quotes and converts them to terms
         data = ' '.join('"%s"' % t if ' ' in t else t for t in terms)
         return data
