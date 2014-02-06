ec64bcaf8b179cbe70450b92bea0852f47734ada
rlskoeser <rebecca.s.koeser@emory.edu>
2014-02-06 13:41:28 -0500

    logic and tests to resize profile pic, generate thumbnail on new image upload [Finishes #64926816 #64927070]

--- belfast/people/models.py
+++ belfast/people/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.core.files import File
+from PIL import Image
 import rdflib
+import tempfile
 
 from belfast.util import rdf_data
 from belfast.groupsheets.rdfmodels import RdfArchivalCollection
@@ -11,6 +14,8 @@ class ProfilePicture(models.Model):
     person_uri = models.URLField(verbose_name='Person', unique=True)
     # TODO: require unique for now so we don't get more than one pic per person?
     img = models.ImageField(upload_to='profile/', verbose_name='Image')
+    thumbnail = models.ImageField(upload_to='profile/thumb/', blank=True,
+        null=True, editable=False)
     title = models.CharField(max_length=255,
         help_text='Title or caption to be shown with the image')
     date = models.CharField(max_length=255,
@@ -25,7 +30,6 @@ class ProfilePicture(models.Model):
     publisher_url = models.URLField(blank=True,
         verbose_name='Publisher Website', help_text='Provide publisher URL, if known')
     permissions = models.TextField(help_text='Statement of Permissions')
-
 
     def __unicode__(self):
         return self.title
@@ -47,6 +51,46 @@ class ProfilePicture(models.Model):
     def collection(self):
         if self.rdfcollection is not None:
             return self.rdfcollection.name
+
+    image_size = (350, 350)
+    thumbnail_size = (128, 128)
+
+    def save(self, *args, **kwargs):
+        # override save method to resize image and generate thumbnail
+
+        image_update = False
+        # if pk is set, this is an update to an existing model;
+        # check if the image is being changed
+        if self.pk:
+            orig = ProfilePicture.objects.get(pk=self.pk)
+            if self.img != orig.img:
+                image_update = True
+
+        if not self.pk or image_update:
+            self.resize_image()
+
+        if self.img and not self.thumbnail or image_update:
+            self.generate_thumbnail()
+
+        super(ProfilePicture, self).save(*args, **kwargs)
+
+
+    def resize_image(self):
+        return self._resize_imagefield(self.image_size, self.img)
+
+    def generate_thumbnail(self):
+        return self._resize_imagefield(self.thumbnail_size, self.thumbnail)
+
+    def _resize_imagefield(self, size, field):
+        image = Image.open(self.img)
+        # NOTE: using thumbnail for both resize/thumb
+        # because it resizes the current image rather than resize,
+        # which returns a new Image object
+        image.thumbnail(size, Image.ANTIALIAS)
+        tmp = tempfile.NamedTemporaryFile(suffix='.png')
+        image.save(tmp.name, 'png')
+        content = File(tmp)
+        field.save('%s.png' % self.rdfperson.slug, content, save=False)
 
 
 
