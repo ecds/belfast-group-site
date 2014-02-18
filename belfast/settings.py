c8c8b21c6f76f245684a95f55410c61efe4c689a
rlskoeser <rebecca.s.koeser@emory.edu>
2014-02-18 17:36:58 -0500

    add flatpages middleware, to make it easier to add new text page

--- belfast/settings.py
+++ belfast/settings.py
@@ -82,6 +82,7 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'belfast.rdf_middleware.RDFaMiddleware'
