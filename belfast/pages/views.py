cc45556ca9d3256c125bc04062ce2dbde24d03a9
rlskoeser <rebecca.s.koeser@emory.edu>
2014-08-11 15:53:32 -0400

    display profile pictures on home page in random order
    needs some refinment from @kevinglover
    
    [#76601086]

--- belfast/pages/views.py
+++ belfast/pages/views.py
@@ -1,5 +1,8 @@
 # Create your views here.
 from django.shortcuts import render
+from belfast.people.models import ProfilePicture
 
 def site_index(request):
-    return render(request, 'pages/site_index.html')
+    pictures = ProfilePicture.objects.all().order_by('?')  # random order
+    return render(request, 'pages/site_index.html',
+                  {'pictures': pictures})
