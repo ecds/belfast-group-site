88dbeb9976b7842c4014e972cc634b37a037b2f5
rlskoeser <rebecca.s.koeser@emory.edu>
2014-08-12 13:02:14 -0400

    generalize flatpage migration to make it easier to add other flatpage content as html

--- belfast/pages/migrations/0002_load_initial_pages.py
+++ belfast/pages/migrations/0002_load_initial_pages.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 import os
-from south.utils import datetime_utils as datetime
+import glob
+import logging
 from south.v2 import DataMigration
 from django.conf import settings
 from django.contrib.flatpages.models import FlatPage
+
+
+logger = logging.getLogger(__name__)
+
 
 class Migration(DataMigration):
 
@@ -12,13 +17,22 @@ class Migration(DataMigration):
         from django.core.management import call_command
         call_command("loaddata", "belfast/pages/fixtures/initial_flatpages.json")
 
-        # now load html content for specific pages
+        # now load html content from files for specific pages
         html_fixture_dir = os.path.join(settings.BASE_DIR, 'pages', 'fixtures',
             'page_content')
 
-        for doc in ['overview']:
-            fp = FlatPage.objects.get(url='/%s/' % doc)
-            with open(os.path.join(html_fixture_dir, '%s.html' % doc)) as html:
+        # iterate over html files in fixture dir and set flatpage content, if possible
+        for doc in glob.glob(os.path.join(html_fixture_dir, '*.html')):
+            basename, ext = os.path.splitext(os.path.basename(doc))
+            try:
+                fp = FlatPage.objects.get(url='/%s/' % basename)
+            except FlatPage.DoesNotExist:
+                # if somehow we have an html file that doesn't correspond
+                # to an existing flatpage, warn and skip
+                logger.warn('No flatpage found for "%s.html"', basename)
+                continue
+
+            with open(doc) as html:
                 fp.content = html.read()
                 fp.save()
 
