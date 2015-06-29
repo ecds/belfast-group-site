fbd34596a9e71de13f1e70df24812f814a2ac306
rlskoeser <rebecca.s.koeser@emory.edu>
2015-06-29 12:29:48 -0400

    Add URIs to site Dublin Core metadata
    - viaf/orcid uris for contributors
    - viaf/dbpedia uris for people with profiles on
      the site, based on the RDF data
    
    [#95274644]

--- belfast/pages/views.py
+++ belfast/pages/views.py
@@ -1,11 +1,20 @@
-# Create your views here.
 from django.shortcuts import render
 from belfast.people.models import ProfilePicture
+from belfast.people.rdfmodels import profile_people
+
+# NOTE: last-modified would be good here, but probably should be based
+# on profile picture modification dates, which is not currently stored
 
 def site_index(request):
     '''Site home page.  Includes a random-order list of profile pictures
     for display at the bottom of the home page.
     '''
     pictures = ProfilePicture.objects.all().order_by('?')  # random order
+    # find people who have profiles on thes ite
+    people = profile_people()
+    # filter to only people with a description
+    people = [p for p in people if p.has_profile and p.picture]
+
     return render(request, 'pages/site_index.html',
-                  {'pictures': pictures})
+                  {'pictures': pictures,
+                   'people': people})
